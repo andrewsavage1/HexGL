0b3d5fd9a031d172b42e61998eae7928d8853453
Andrew Savage <andrewsavage@google.com>
2025-04-08 20:14:59 -0400

    fix touch logic

--- bkcore.coffee/Utils.js
+++ bkcore.coffee/Utils.js
@@ -228,7 +228,7 @@
      */
 
     Utils.isTouchDevice = function() {
-      return ('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
+      return false;
     };
 
     return Utils;
